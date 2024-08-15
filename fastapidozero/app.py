f9d9c3ace78c1e8ba1355a419f64cc1edfae1b84
Kaori Utsunomya <j.kaoriutsunomya@gmail.com>
2024-08-15 18:42:30 -0300

    Aula 02 -

--- fastapidozero/app.py
+++ fastapidozero/app.py
@@ -1,8 +1,25 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
+# a barra seria a raíz do site (root)
+# @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
+# def read_root():
+#     return {'message': 'Olá Mundo!'} #deixar de comentar ctrl + k +c
 
-@app.get('/')  # a barra seria a raíz do site (root)
+
+@app.get(
+    '/', response_class=HTMLResponse
+)  # a barra seria a raíz do site (root)
 def read_root():
-    return {'message': 'Olá Mundo!'}
+    return """
+    <html>
+        <head>
+            <title> Nosso Olá Mundo! </title>
+        <body>
+            <h1> Olá Mundo! :) </h1>
+        </body>
+        </head>
+    </html>
+    """
--- tests/test_app.py
+++ tests/test_app.py
@@ -4,10 +4,20 @@ from fastapi.testclient import TestClient
 
 from fastapidozero.app import app
 
+# def test_read_root_deve_retornar_ok_e_ola_mundo():
+#     client = TestClient(app) #arragen ou organização
 
-def test_read_root_deve_retornar_ok_e_ola_mundo():
-    client = TestClient(app)  # arragen ou organização
+#     response = client.get('/') #act ou ação
 
-    response = client.get('/')  # act ou ação
+#     assert response.status_code == HTTPStatus.OK #assert garantir
+#     assert response.json() == {'message': 'Olá Mundo!'} #assert
 
-    assert response.status_code == HTTPStatus.OK  # assert garantir
+
+def test_read_root_deve_retornar_ola_mundo():
+    client = TestClient(app)  # Arrange -> setar as coisas
+
+    response = client.get('/')  # Act -> replicar a ação que o cliente faria
+
+    # Assert -> assegurar que o comportamento esperado está acontecendo
+    assert response.status_code == HTTPStatus.OK
+    assert '<h1> Olá Mundo! :) </h1>' in response.text
